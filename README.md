c17e29d76f345816fee7ee82a8d7f57c1039609c
paulo.weber <paulo.weber@senior.com.br>
2021-03-02 21:45:14 -0300

    notification services

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Repository to push notification service from https://www.udemy.com/course/server
 - Serverless framework
 - SES
 - Lambdas in Node.js
+
+
+Create a .env file with MAIL_SOURCE variable and put your email there, if your account is in sandbox mode, it's necessary to register this email at AWS Verified Mails and confirm it(you'll receive a mail from AWS to confirm, so you need a valid email)
--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,43 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({
+  'region': 'sa-east-1'
+});
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const record = event.Records[0];
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
+  const params = {
+    Source: process.env.MAIL_SOURCE,
+    Destination: {
+      ToAddresses: [
+        recipient
+      ]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: body
+        }
+      },
+      Subject: {
+        Data: subject
+      }
+    },
+  };
+
+  console.log(params);
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
